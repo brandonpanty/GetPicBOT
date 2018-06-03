485e8153cd9a628f8c6eba1029c2b33eb6bbd545
Sam Liu <hi1999@gmail.com>
2018-06-03 20:28:32 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -17,7 +17,7 @@ const { Client } = require('pg');
 //const client = new Client({
 //    connectionString: process.env.DATABASE_URL,
 //    ssl: true,
-});
+//});
 //client.connect();
 
 var myDate = new Date();
