066c191f922b660a7cbffa1e570af0de450db94e
Sam Liu <hi1999@gmail.com>
2018-06-03 21:15:49 +0800

    Update Schedule_Report.js

--- Schedule_Report.js
+++ Schedule_Report.js
@@ -8,20 +8,13 @@ const { Client } = require('pg');
 //Line主動推播測試
 var linebot = require('linebot');
 var express = require('express');
-//var request = require('request');
 console.log('宣告Line BOT');
 var bot = linebot({
     channelId: '1585073032',
     channelSecret: '0af678b6490a7452cd453250d9bd6998',
     channelAccessToken: 'IOHrMu7KzhjGMinqIDTpoXrxN/NlHFzF3ni7SYAAjN0SShmin7XW/5omFuSGq4tJuTvIdMstBWVpfADfORx7cT0/97Tkx5lmHY2z+77az/1hXYQfRCJGGUbOlSg15drJNNaojTkOSK3bU982zARsZQdB04t89/1O/w1cDnyilFU='
 });
-//var options = {
-//    url: 'https://api.imgur.com/3/album/BJNxWqK/images',
-//    headers: { 'Authorization': 'Client-ID d09fd3905abd246' }
-//};
-//function callback(error, response, body) {
-//    if (!error && response.statusCode == 200) {
-//        var info = JSON.parse(body);
+
         const client = new Client({
             connectionString: process.env.DATABASE_URL,
             ssl: true,
@@ -97,7 +90,5 @@ var bot = linebot({
             }
             client.end();
         });
-//    }
-//}
-//request(options, callback);
+
 console.log('==================');
