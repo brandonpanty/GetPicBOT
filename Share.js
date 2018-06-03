3a53b1b6f7a2c0cbe491a356b4a5511beff7572a
brandonpanty <39869446+brandonpanty@users.noreply.github.com>
2018-06-03 20:37:25 +0800

    Update Share.js

--- Share.js
+++ Share.js
@@ -38,21 +38,71 @@ function callback(error, response, body) {
                 console.log('==>push ui:' + ui);
                 var imgLink = info.data[Math.floor(Math.random() * info.data.length)].link;
                 bot.push(ui, {
-                            type: 'template',
-                            altText: 'this is a confirm template',
-                            template: {
-                                type: 'confirm',
-                                text: 'Are you sure?',
-                                actions: [{
-                                type: 'message',
-                                label: 'Yes',
-                                text: 'yes'
-                            }, {
-                                type: 'message',
-                                label: 'No',
-                                text: 'no'
-                            }]
-                        }
+                            "type": "template",
+                            "altText": "this is a carousel template",
+                            "template": {
+                                "type": "carousel",
+                                "columns": [
+                                {
+                                    "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                                    "imageBackgroundColor": "#FFFFFF",
+                                    "title": "this is menu",
+                                    "text": "description",
+                                    "defaultAction": {
+                                        "type": "uri",
+                                        "label": "View detail",
+                                        "uri": "http://example.com/page/123"
+                                    },
+                                    "actions": [
+                                    {
+                                        "type": "postback",
+                                        "label": "Buy",
+                                        "data": "action=buy&itemid=111"
+                                    },
+                                    {
+                                        "type": "postback",
+                                        "label": "Add to cart",
+                                        "data": "action=add&itemid=111"
+                                    },
+                                    {
+                                        "type": "uri",
+                                        "label": "View detail",
+                                        "uri": "http://example.com/page/111"
+                                    }
+                                ]
+                              },
+                              {
+                                "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                                "imageBackgroundColor": "#000000",
+                                "title": "this is menu",
+                                "text": "description",
+                                "defaultAction": {
+                                    "type": "uri",
+                                    "label": "View detail",
+                                    "uri": "http://example.com/page/222"
+                                },
+                                "actions": [
+                                    {
+                                        "type": "postback",
+                                        "label": "Buy",
+                                        "data": "action=buy&itemid=222"
+                                    },
+                                    {
+                                        "type": "postback",
+                                        "label": "Add to cart",
+                                        "data": "action=add&itemid=222"
+                                    },
+                                    {
+                                        "type": "uri",
+                                        "label": "View detail",
+                                        "uri": "http://example.com/page/222"
+                                    }
+                                ]
+                              }
+                          ],
+                          "imageAspectRatio": "rectangle",
+                          "imageSize": "cover"
+                      }
                 });
                                 console.log('\t==>push [' + imgLink+'] ok');
             }
