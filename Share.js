b256f6290d8017dc0474ad31145b7b96b378b6bf
brandonpanty <39869446+brandonpanty@users.noreply.github.com>
2018-06-03 12:27:47 +0000

    Update Share.js

--- Share.js
+++ Share.js
@@ -38,39 +38,8 @@ function callback(error, response, body) {
                 console.log('==>push ui:' + ui);
                 var imgLink = info.data[Math.floor(Math.random() * info.data.length)].link;
                 bot.push(ui, {
-                    "type": "template",
-                    "altText": "this is a carousel template",
-                    "template": {
-                        "type": "carousel",
-                        "columns": [
-                        {
-                            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-                            "imageBackgroundColor": "#FFFFFF",
-                            "title": "this is menu",
-                            "text": "description",
-                            "defaultAction": {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/123"
-                            },
-                            "actions": [
-                                {
-                                    "type": "postback",
-                                    "label": "Buy",
-                                    "data": "action=buy&itemid=111"
-                                },
-                                {
-                                    "type": "postback",
-                                    "label": "Add to cart",
-                                    "data": "action=add&itemid=111"
-                                },
-                                {
-                                    "type": "uri",
-                                    "label": "View detail",
-                                    "uri": "http://example.com/page/111"
-                                }
-                            ]
-                         });
+                    
+                });
                                 console.log('\t==>push [' + imgLink+'] ok');
             }
             client.end();
