1ab17ccf251caf8d539a8e1c306cf17a34e2d605
brandonpanty <39869446+brandonpanty@users.noreply.github.com>
2018-06-11 10:15:48 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -139,16 +139,16 @@ bot.on('message', function (event) {
        // function callback(error, response, body) {
        //     if (!error && response.statusCode == 200) {
        //         var info = JSON.parse(body);
-                //console.log(info.data[Math.floor(Math.random() * info.data.length)].link);
+                console.log(info.data[Math.floor(Math.random() * info.data.length)].link);
                 console.log('傳遞卡片');
                 //需要再加入隨機功能
                 event.reply({
                     "type": "image",
 //                    https://imgur.com/1i2DQft
-                    //"originalContentUrl": info.data[Math.floor(Math.random() * info.data.length)].link,
-                    "originalContentUrl": 'https://i.imgur.com/Mki9oX3.jpg',
-                    //"previewImageUrl": info.data[Math.floor(Math.random() * info.data.length)].link
-                    "previewImageUrl": 'https://i.imgur.com/Mki9oX3.jpg'
+                    "originalContentUrl": info.data[Math.floor(Math.random() * info.data.length)].link,
+                    //"originalContentUrl": 'https://i.imgur.com/Mki9oX3.jpg',
+                    "previewImageUrl": info.data[Math.floor(Math.random() * info.data.length)].link
+                    //"previewImageUrl": 'https://i.imgur.com/Mki9oX3.jpg'
                 });
        //     }
        // }
